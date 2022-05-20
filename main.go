67f412f37cc5c7078836917619cd13d087d11659
venomuz <venom.uzz@mail.ru>
2022-05-20 09:58:34 +0500

    adds

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-
-
 package main
 
 import (
@@ -7,7 +5,7 @@ import (
 	ss "project1/Strucks"
 )
 
-func main(){
-	person := ss.Customer{FirstName: "Davron",LastName: "Kholmatov",Username: "venom",Phones: }
-	nums := ss.[]ss.Phone{Numbers}
+func main() {
+	person := ss.Customer{FirstName: "Davron", LastName: "Kholmatov", Username: "venom", Phones: []ss.Phone{{UserID: "asd"}}}
+	fmt.Println(person)
 }
